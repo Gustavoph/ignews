1abed31e7daeb5bcdde8303d3993a5fc61696e10
Gustavo Oliveira <gustavo.gabriel@beresolution.com>
2022-02-18 10:11:36 -0300

    :sparkles: auth
    add a next auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,27 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 import styles from './styles.module.scss'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession()
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signOutButton}>
+  return session ? (
+    <button 
+      type="button" 
+      className={styles.signOutButton}
+      onClick={() => signOut()}
+    >
       <FaGithub/>
-      Gustavo Oliveira
+      {session.user.name}
       <FiX className={styles.closeIcon}/>
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button 
+      type="button" 
+      onClick={() => signIn('github')}
+      className={styles.signInButton}
+    >
       <FaGithub />
       Sign in with GitHub
     </button>
