5f6ce30d9468b7ff423a6d41b0055fad644c31cd
Gustavo Oliveira <gustavo.gabriel@beresolution.com>
2022-02-25 10:01:01 -0300

    :sparkles: add page post

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,20 @@
 import Image from 'next/image';
-import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
+import { SignInButton } from '../SignInButton';
+import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
-  
   return(
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <Image src="/images/logo.svg" width="110" height="31"  alt="ig.news" />
         <nav>
-          <a href="" className={styles.active}>Home</a>
-          <a href="">Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,22 @@
+import Link from 'next/link';
 import Head from 'next/head';
+import { GetStaticProps } from 'next';
+import { RichText } from 'prismic-dom'
+import Prismic from '@prismicio/client'
 import styles from './styles.module.scss';
+import { getPrismicClient } from '../../services/prismic';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -10,23 +25,49 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a>
-            <time>12 de março de 2021</time>
-            <strong>Creatinf a Monorepo with Lerna & Yarn Workspaces</strong>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex aperiam reprehenderit iusto velit vero ullam?</p>
-          </a>
-          <a>
-            <time>12 de março de 2021</time>
-            <strong>Creatinf a Monorepo with Lerna & Yarn Workspaces</strong>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex aperiam reprehenderit iusto velit vero ullam?</p>
-          </a>
-          <a>
-            <time>12 de março de 2021</time>
-            <strong>Creatinf a Monorepo with Lerna & Yarn Workspaces</strong>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex aperiam reprehenderit iusto velit vero ullam?</p>
-          </a>
+          { posts.map(post => {
+            return (
+              <Link href={`/posts/${post.slug}`}>
+                <a key={post.slug}>
+                  <time>{post.updatedAt}</time>
+                  <strong>{post.title}</strong>
+                  <p>{post.excerpt}</p>
+                </a>
+              </Link>
+            )
+          })}
         </div>
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient()
+
+  const response = await prismic.query<any>([
+    Prismic.predicates.at('document.type', 'post')
+  ], {
+    fetch: ['post.title', 'post.content'],
+    pageSize: 100
+  })
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  })
+
+  return {
+    props: {
+      posts
+    }
+  }
 }
